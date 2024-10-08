7e3e2407662e216ada66f20aff8d15e0a4978bdb
GokbulakYasin <yasingokbulak08@gmail.com>
2024-10-08 21:23:51 +0300

    Yaş Hesaplama

--- yashesaplama.py
+++ yashesaplama.py
@@ -1,4 +1,4 @@
-year = int(input("Bulunduğunuz yılı giriniz. : "))
-born = int(input("Doğduğunuz yılı giriniz : "))
-
-print("Yaşınız" , year-born)
+dogum_yili = int(input("Doğum yılınızı girin: "))
+su_an_yili = 2024  # Güncel yılı burada yazabilirsiniz
+yas = su_an_yili - dogum_yili
+print("Yaşınız:", yas)
